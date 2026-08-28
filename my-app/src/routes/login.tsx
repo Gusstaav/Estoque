3855d5b10e36aae3f09279dbb48aff3245a15c67
Gusstaav <83920275+Gusstaav@users.noreply.github.com>
2026-08-28 06:52:45 +0000

    firt commit

---
+++ my-app/src/routes/login.tsx
@@ -0,0 +1,16 @@
+import react from 'react'
+import './login.css'
+
+function Login(){
+    return(
+        <html>
+           <div id="container" className='container'>
+                <h1>Login</h1>
+                <h1>Login</h1>
+                <input name="user" type="text" placeholder="Name:"/>
+            </div> 
+        </html>
+    )
+}
+
+export default Login
